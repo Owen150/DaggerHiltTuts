c058fe758169c6a79f5e864aab23b19f37d37ca1
Michael Owen Oduor <michaeloduor50@gmail.com>
2022-03-28 19:49:12 +0300

    Performed test on Main Activity

--- app/src/main/java/com/example/daggerhilt/MainActivity.kt
+++ app/src/main/java/com/example/daggerhilt/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.daggerhilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+//To be able to inject anything into an activity, fragment, etc.
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var testString: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "Test string from MainActivity: $testString")
     }
 }
